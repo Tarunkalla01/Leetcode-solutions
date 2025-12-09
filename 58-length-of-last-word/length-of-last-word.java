579d204bdeaa64b20224affc7cca4b4bff1cf503
Tarun kalla <tarunkalla0125@gmail.com>
2025-12-09 10:50:31 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (52.46%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count=0;
+        for(int i=s.length()-1;i>=0;i--){
+            
+            if(count>0 && s.charAt(i)== ' '){
+                
+               break ;
+            }
+             if(count==0 && s.charAt(i)== ' '){
+                
+               continue;
+            }
+            count++;
+
+        }
+        return count;
+    }
+}
