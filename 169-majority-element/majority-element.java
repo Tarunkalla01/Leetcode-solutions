9849cc0422ac5d4102da451ae236cf272fe93e1f
Tarun kalla <tarunkalla0125@gmail.com>
2025-12-18 16:40:02 +0530

    Time: 1 ms (99.84%) | Memory: 55.8 MB (8.34%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       
-      Arrays.sort(nums);
-      return nums[nums.length/2];
+        // Boyle moore voting Algo 
+        int count=0;
+        int cand = nums[0];
+        for(int i=0;i<nums.length;i++){
+           
+            if(nums[i]==cand){
+                count++;
+            }
+           else{
+            count--;
+           }
+             if(count==0){
+                cand = nums[i];
+                count=1;
+            }
+           
+        }
+        return cand; 
     }
 }
