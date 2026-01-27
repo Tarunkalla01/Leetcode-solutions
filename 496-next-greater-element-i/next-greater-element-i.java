bd0d1a53556f55f86f3ed68a2be94f532f3ad11f
Tarun kalla <tarunkalla0125@gmail.com>
2026-01-27 17:38:43 +0530

    Time: 5 ms (17.94%) | Memory: 46.4 MB (8.42%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -1,27 +1,46 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int ans[] = new int[nums1.length];
-   
-        for(int i = 0 ; i<nums1.length ; i++){
+       Stack<Integer> st = new Stack<>();
+    HashMap<Integer , Integer> nextg = new HashMap<>();
+    HashMap<Integer , Integer> mapping = new HashMap<>();
+     int ans[] = new int[nums1.length];
+    // store the value , indexes in map 
+    for(int i=0;i<nums2.length ; i++){
+        mapping.put(nums2[i] , i);
+    }
 
-            int x=nums1[i];
-            ans[i]=-1;
+    // store indexes of next greater
+    for(int i=0;i<nums2.length ; i++){
 
-            for(int j=0 ; j<nums2.length ; j++){
+      
 
-                if(x==nums2[j]){
-                    for(int temp = j+1; temp<nums2.length;temp++){
-                    if(nums2[temp]>x){
-
-                        ans[i]=nums2[temp];
-                        break;
-                        
-                    }
-                    
-                }
-            }
+        while(!st.isEmpty() && nums2[i]>nums2[st.peek()]){
+            int temp = st.peek();
+            st.pop();
+            nextg.put(temp ,i);
         }
-    }   
+        st.push(i);
+    } 
+
+    // remaining stack element
+    while(!st.isEmpty()){
+        int t = st.peek();
+        st.pop();
+        nextg.put(t,-1);
+    }
+
+    //
+   
+    for(int i = 0 ; i<nums1.length ; i++){
+        int index = mapping.get(nums1[i]); 
+        int ng = nextg.get(index);
+        if(ng==-1){
+            ans[i]=-1;
+        }
+        else{
+            ans[i] = nums2[ng];
+        }
+    }
     return ans;
     }
 }
