bdbe93347567a2782cd819c7eb8d7f5b8069e362
Tarun kalla <tarunkalla0125@gmail.com>
2026-02-04 15:23:30 +0530

    Time: 6 ms (25.26%) | Memory: 43.9 MB (11.42%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -2,19 +2,10 @@ class Solution {
     public int hIndex(int[] citations) {
         
         Arrays.sort(citations);
-
-        int Start = citations[0];
-        int end = citations[citations.length-1];
-        
-        for(int i =end ; i>= 0 ; i--){
-            int cnt=0;
-            for(int j=0;j<citations.length;j++){
-                if(citations[j]>=i){
-                    cnt++;
-                }
-            }
-            if(cnt>=i){
-                return i;
+        int n = citations.length;
+        for(int i=0;i<n;i++){
+            if(citations[i]>=n-i){
+                return n-i;
             }
         }
         return 0;
